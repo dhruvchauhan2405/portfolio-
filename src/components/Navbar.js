a8ca9b5eda359e175c2c7fdb12274b9c029fee12
dhruvchauhan2405 <dhruvsingh2405@gmail.com>
2023-08-06 12:02:17 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,6 +82,15 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
