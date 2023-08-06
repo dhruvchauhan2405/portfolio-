eae99dd8d8e10304cf65c3756ad04a525efd039a
dhruvchauhan2405 <dhruvsingh2405@gmail.com>
2023-08-06 13:42:00 +0530

    changes

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,12 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ["Developer", "Front End Developer"],
+        strings: [
+          "Front End Developer",
+          "Content Creator",
+          "Video Editor",
+          "Graphic Designer",
+        ],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
